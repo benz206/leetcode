bc7a5633b0df45832953ef1cd39abb2aa4586408
Ben Zhou <ben.zhou.2006@gmail.com>
2025-12-25 15:32:14 -0500

    Add another sol

---
+++ solutions/3075-happiness/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end(), greater<int>());
+
+        long long sum = 0;
+
+        for (int i = 0; i < k; ++i) {
+            sum += max(0, happiness[i] - i) ;
+        }
+
+        return sum;
+    }
+};
